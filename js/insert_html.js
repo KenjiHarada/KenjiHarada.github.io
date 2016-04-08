c6fdeca9e6feda4e837ffa94bcd50e222b54bc59
Kenji Harada <harada@acs.i.kyoto-u.ac.jp>
2016-04-08 22:16:18 +0900

    fixed

--- js/insert_html.js
+++ js/insert_html.js
@@ -1,4 +1,4 @@
 $(function() {
-    $("#footer").load("http://kenjiharada.github/html/footer.html");
-    $("#footer_en").load("http://kenjiharada.github/html/footer_en.html");
+    $("#footer").load("http://kenjiharada.github.io/html/footer.html");
+    $("#footer_en").load("http://kenjiharada.github.io/html/footer_en.html");
 });
