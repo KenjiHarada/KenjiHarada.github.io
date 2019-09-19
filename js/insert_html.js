513b58fd9c174ad1e5c3a92a63ed53378e71f33d
Kenji Harada <harada@acs.i.kyoto-u.ac.jp>
2019-09-19 11:03:38 +0900

    update footer

--- js/insert_html.js
+++
@@ -1,4 +0,0 @@
-$(function() {
-    $("#footer").load("http://www-fcs.acs.i.kyoto-u.ac.jp/~harada/html/footer.html");
-    $("#footer_en").load("http://www-fcs.acs.i.kyoto-u.ac.jp/~harada/html/footer_en.html");
-});
